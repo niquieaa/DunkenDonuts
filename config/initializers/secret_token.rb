a1709eae2b04d1c4cd832207f70c8ab2522f3039
Niquieaa <nicole.arboleda@gmail.com>
2012-08-19 23:29:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DunkenDonuts::Application.config.secret_token = '0c077dee1960d20048f22bda7b4b09fb007fcf840613a1befaa8985dc4a86d68fc7114b8b14793fbb19045a3b0f626eceb5d9ccc0fe8c5498eee76ab991848fc'
